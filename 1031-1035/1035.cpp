4e842efe41b916bb474b87560d25e765f4b60d11
Miantang <antisaber@gmail.com>
2015-05-28 00:39:06 +0800

    mergesort

--- 1031-1035/1035.cpp
+++ 1031-1035/1035.cpp
@@ -18,6 +18,7 @@ bool cmp(int *A, int *B, int& n)
 }
 void insertSort2(int *A, int& n, int& i, int* CMP, bool once)
 {
+    bool flag = false;
     for (int j = i; j > 0; j--) 
     {
         if(A[j] < A[j-1])
@@ -25,18 +26,24 @@ void insertSort2(int *A, int& n, int& i, int* CMP, bool once)
             swap(A[j], A[j-1]);
         }
         else
+        {
+            if(j == i)
+                flag = true;
+            else
+                flag = false; 
             break;
+        } 
     }
-    if(once)
-        return;
-    if(cmp(A, CMP, n))
-        return;
     i++;
-    if(i >= n)
+    if(once && !flag)
+        return;
+    if(cmp(A, CMP, n) && !flag)
+        return;
+    if(i > n)
     {
         return;
     }
-    // insertSort2(A, n, i);
+     insertSort2(A, n, i, CMP, false);
 }
 void merge(int* A, int first, int mid, int end, int* B)
 {
@@ -62,16 +69,28 @@ void merge(int* A, int first, int mid, int end, int* B)
         else                A[k] = B[j++];
     }
 }
-void mergeSort(int *A, int& n, int *B/*, int first, int mid, int end, int* CMP, bool once*/)
+void mergeSort(int *A, int& n, int *B, int& sz,  int* CMP, bool once)
 {
-    for(int sz = 1; sz <= n-1; sz = sz+sz)
+//    for(int sz = 1; sz <= n-1; sz = sz+sz)
+//    {
+//        for(int i = 0; i < n -sz; i += sz+sz)
+//        {
+//            merge(A, i, i+sz-1, min(i+sz+sz-1, n-1), B);
+//        }
+//    }
+    for(int i = 0; i < n -sz; i += sz+sz)
     {
-        for(int i = 0; i < n -sz; i += sz+sz)
-        {
-            merge(A, i, i+sz-1, min(i+sz+sz-1, n-1), B);
-        }
+        merge(A, i, i+sz-1, min(i+sz+sz-1, n-1), B);
     }
-
+    sz = sz + sz;
+    if(once)
+        return;
+    if(cmp(A, CMP, n))
+        return;
+    if(sz > n-1)
+        return;
+    mergeSort(A, n, B, sz, CMP, once);
+    
 }
 int main() {
     #ifndef ONLINE_JUDGE
@@ -82,19 +101,40 @@ int main() {
     #endif
     int N;
     scanf("%d", &N);
-    int arr[N], B[N], CMP[N];
+    int arr[N], arr2[N], B[N], CMP[N];
     for (int i = 0; i < N; ++i) 
         scanf("%d", &arr[i]);
-    int insOrder = 1;
-    if(N != 1)
-        // insertSort2(arr, N, insOrder);
-        mergeSort(arr, N,  B);
     for (int i = 0; i < N; ++i) 
+        arr2[i] = arr[i];
+    for (int i = 0; i < N; ++i) 
+        scanf("%d", &CMP[i]);
+    int oder = 1;
+    if(N != 1)
+        mergeSort(arr, N,  B, oder, CMP, false);
+    if(cmp(arr, CMP, N))
     {
-        if(i != 0)
-            printf(" ");
-        printf("%d", arr[i]);
+        mergeSort(arr, N, B, oder, CMP, true);
+        printf("Merge Sort\n");
+        for (int i = 0; i < N; ++i) 
+        {
+            if(i != 0)
+                printf(" ");
+            printf("%d", arr[i]);
+        }
     }
+    else{
+        oder = 1;
+        if(N != 1)
+            insertSort2(arr2, N, oder, CMP, false);
+        insertSort2(arr2, N, oder, CMP, true);
+        printf("Insertion Sort\n"); 
+        for (int i = 0; i < N; ++i) 
+        {
+            if(i != 0)
+                printf(" ");
+            printf("%d", arr2[i]);
+        }
+    } 
     
     return 0;
 }
